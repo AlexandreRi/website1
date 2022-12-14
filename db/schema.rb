a6f401a7d6f961535d57a768b39f07877ad9b80c
Alexandre Rinaldi <alexandre.rinaldi@essec.edu>
2022-12-14 18:36:41 +0100

    new gems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_115933) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_14_123640) do
   create_table "investments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
